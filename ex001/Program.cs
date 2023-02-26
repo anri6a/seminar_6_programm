36094e25075bfdc7d20a70a904073d0e3d191a69
Andrei Shyrakou <anri6a@gmail.com>
2023-02-26 14:57:18 +0300

    add InputNumber

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -2,3 +2,10 @@
 // определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, 3] -> да
+int InputNumber (string text)
+{
+    Console.WriteLine (text);
+    int number = new Convert.ToInt32(Console.ReadLine ());
+    return number;
+}
+int InputNumer = InputNumber ("Введите искомое число");
